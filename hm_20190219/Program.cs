0e7f75319eb87f4d8dc9e6406a6218c9d84c64a9
dm <muk_dinara@mail.ru>
2019-02-20 18:14:39 +0600

    5-8

--- hm_20190219/Program.cs
+++ hm_20190219/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace hm_20190219
 {
     class Program
@@ -29,10 +30,15 @@ namespace hm_20190219
             //}
             //Console.WriteLine(a1<a2);
 
-            Money a = new Money(100000, (currency)0);
-            Money b = new Money(200, (currency)1);            
-            a = a + new Money(new ConvertMoney(b, 375.5).Sum, a.Currency);
-            Console.WriteLine(a.getInfo());
+            //Money a = new Money(100000, (currency)0);
+            //Money b = new Money(200, (currency)1);            
+            //a = a + new Money(new ConvertMoney(b, 375.5).Sum, a.Currency);
+            //Console.WriteLine(a.getInfo());
+
+            //double x = 0.1456;
+            //Decimal_ d = (Decimal_)x;
+
+            
             
         }
         
@@ -275,8 +281,192 @@ namespace hm_20190219
                 sum2 += y.arr[i];
             }
             return sum1 >= sum2;
+        }        
+
+        public int Length()
+        {
+            return arr.Length;
         }
 
-
     }
+
+    //5.	Класс – одномерный массив. 
+    //Дополнительно перегрузить следующие операции: [] – доступ по индексу; == – проверка на равенство; != – проверка на неравенство; + – объединение массивов
+    public class B
+    {
+        private int[] arr = null;
+        public B()
+        {
+            arr = new int[10];
+        }
+        public B(int num)
+        {
+            arr = new int[num];
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+        public static bool operator ==(B x, B y)
+        {
+            if (x.arr.Length != y.arr.Length)
+                return false;
+            else
+            {
+                for (int i = 0; i < x.arr.Length; i++)
+                {
+                    if (x.arr[i] != y.arr[i])
+                        return false;
+                }
+                return true;
+            }
+        }
+        public static bool operator !=(B x, B y)
+        {
+            if (x.arr.Length != y.arr.Length)
+                return true;
+            else
+            {
+                for (int i = 0; i < x.arr.Length; i++)
+                {
+                    if (x.arr[i] != y.arr[i])
+                        return true;
+                }
+                return false;
+            }
+        }
+        public static B operator +(B x, B y)
+        {
+            if (x.arr.Length != y.arr.Length)
+                throw new Exception("Разные размры у массивов");
+            B z = new B(x.arr.Length);
+            for (int i = 0; i < x.arr.Length; i++)
+            {
+                z[i] = x[i] + y[i];
+            }
+            return z;
+        }
+    }
+
+    //6.	Создать класс Decimal для работы с без знаковыми целыми десятичными числами, 
+    //используя для представления массив из 100 элементов типа чар, каждый из которых является десятичной цифрой. 
+    //Реализовать арифметические операции + - * /
+    public class Decimal_
+    {
+        public decimal d { get; set; }
+        public Decimal_() { }
+        public Decimal_(decimal x)
+        {
+            d = x;
+        }
+        public static explicit operator Decimal_(int x)
+        {
+            return new Decimal_ { d = x };
+        }
+        public static explicit operator Decimal_(double x)
+        {
+            return new Decimal_ { d = (int)x };
+        }
+
+        public static Decimal_ operator +(Decimal_ a, Decimal_ b)
+        {
+            return new Decimal_(a.d + b.d);
+        }
+        public static Decimal_ operator -(Decimal_ a, Decimal_ b)
+        {
+            return new Decimal_(a.d - b.d);
+        }
+        public static Decimal_ operator *(Decimal_ a, Decimal_ b)
+        {
+            return new Decimal_(a.d * b.d);
+        }
+        public static Decimal_ operator /(Decimal_ a, Decimal_ b)
+        {
+            return new Decimal_(a.d / b.d);
+        }
+    }
+
+    //7.	Создать структуру Complex с перегруженными операциями, а также с возможностью приведения типа double->complex. 
+    //Должны быть реализованы также ToString(), Equals(), ==, !=. Сравнить производительность в случае реализации Complex как класса и как структуры.
+    public class Complex_
+    {
+        public double num { get; set; }
+        public static explicit operator Complex_(double x)
+        {
+            return new Complex_ { num = x };
+        }
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", num, 0);
+        }
+        public override bool Equals(object obj)
+        {
+            return num == ((Complex_)obj).num;
+        }
+        public static bool operator ==(Complex_ a, Complex_ b)
+        {
+            return a.num == b.num;
+        }
+        public static bool operator !=(Complex_ a, Complex_ b)
+        {
+            return a.num != b.num;
+        }
+    }
+
+    //8.	Создать класс Frac с перегруженными операциями + - * / , а также с возможностью приведения типа Frac->double. 
+    //Должны быть реализованы также ToString(), Equals(), ==, !=. Вычислить значение полинома в точке. Все коэффициенты и x должны иметь тип Frac. 
+    //Сравнить производительность в случае реализации Frac как класса и как структуры.
+    public class Frac
+    {
+        private int x, y;
+        public Frac(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public static explicit operator double(Frac f)
+        {
+            return (double)f.x / f.y;
+        }
+        public static Frac operator +(Frac a, Frac b)
+        {
+            return new Frac(a.x * b.y + b.x * a.y, a.y * b.y);
+        }
+        public static Frac operator -(Frac a, Frac b)
+        {
+            return new Frac(a.x * b.y - b.x * a.y, a.y * b.y);
+        }
+        public static Frac operator *(Frac a, Frac b)
+        {
+            return new Frac(a.x*b.x, a.y*b.y);
+        }
+        public static Frac operator /(Frac a, Frac b)
+        {
+            return new Frac(a.x*b.y, a.y*b.x);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", x, y);
+        }
+        public override bool Equals(object obj)
+        {
+            return (x == ((Frac)obj).x && y == ((Frac)obj).y) || (double)x / y == (double)obj;
+        }
+        public static bool operator ==(Frac a, Frac b)
+        {
+            return (a.x == b.x && a.y == b.y) || (double)a.x/a.y==(double)b.x/b.y;
+        }
+        public static bool operator !=(Frac a, Frac b)
+        {
+            return (a.x != b.x || a.y != b.y) || (double)a.x / a.y != (double)b.x / b.y;
+        }
+    }
+
 }
